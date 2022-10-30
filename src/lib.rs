23bd569a42b5bd32d99a908cb377120eb489d194
Jatinderjit Singh <jatinderjit89@gmail.com>
2022-10-30 11:12:30 +0530

    Implement brainfuck without loops

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use std::io::{self, stdin, stdout, Read, Write};
+
+const RAM_SIZE: usize = 30000;
+
+pub type Error = io::Error;
+
+pub struct Program {
+    memory: [u8; RAM_SIZE],
+    pointer: usize,
+    program_counter: usize,
+    source: Vec<u8>,
+}
+
+impl Program {
+    pub fn new(source: Vec<u8>) -> Self {
+        Self {
+            memory: [0; RAM_SIZE],
+            pointer: 0,
+            program_counter: 0,
+            source,
+        }
+    }
+
+    pub fn run(&mut self) -> Result<(), Error> {
+        loop {
+            if self.program_counter == self.source.len() {
+                return Ok(());
+            }
+            match self.source[self.program_counter] {
+                b',' => stdin().read_exact(&mut self.memory[self.pointer..self.pointer + 1])?,
+                b'.' => {
+                    stdout().write(&self.memory[self.pointer..self.pointer + 1])?;
+                }
+                b'+' => self.memory[self.pointer] += 1, //TODO: handle underflow
+                b'-' => self.memory[self.pointer] -= 1, //TODO: handle overflow
+                b'>' => self.pointer += 1,              // TODO: handle memory overflow
+                b'<' => {
+                    if self.pointer == 0 {
+                        self.pointer = self.source.len() - 1;
+                    } else {
+                        self.pointer -= 1;
+                    }
+                }
+                b'[' => todo!(),
+                b']' => todo!(),
+                _ => {}
+            };
+            self.program_counter += 1;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use brainfuck::{Error, Program};
+
+fn main() -> Result<(), Error> {
+    let mut source = vec![b'+'; 200];
+    source[97] = b'>';
+    source[197] = b'.';
+    source[198] = b'<';
+    source[199] = b'.'; // Should print 'ca'
+    let mut program = Program::new(source);
+    program.run()?;
+    return Ok(());
 }
