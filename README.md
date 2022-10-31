a509a14331ea37d1b6f19793d6c4a8f3d5b5695b
Jatinderjit Singh <jatinderjit89@gmail.com>
2022-10-31 17:58:01 +0530

    Read source from files and add examples

---
+++ README.md
@@ -0,0 +1,4 @@
+# Brainfuck
+
+This follows the blog
+[Compiling Brainfuck Code](https://rodrigodd.github.io/2022/10/21/bf_compiler-part1.html).
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use brainfuck::{Error, Program};
 
 fn main() -> Result<(), Error> {
-    let mut source = vec![b'+'; 200];
-    source[97] = b'>';
-    source[197] = b'.';
-    source[198] = b'<';
-    source[199] = b'.'; // Should print 'ca'
-    let mut program = Program::new(vec![b'+', b'[', b',', b'.', b']']);
+    let file_name = std::env::args().nth(1).expect("Please enter the file path");
+    let source = std::fs::read(&file_name).expect("Error reading file");
+    let mut program = Program::new(source);
     program.run()?;
     return Ok(());
 }
